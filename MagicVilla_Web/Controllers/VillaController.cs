baee6f02b0c87f6713afcc06078119e51ccf2f84
Nazrah Shera <nazrah.shera@gmail.com>
2025-06-02 22:18:22 +0500

    Delete Villa UI done.

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -83,5 +83,34 @@ namespace MagicVilla_Web.Controllers
         }
 
 
+
+        public async Task<IActionResult> DeleteVilla(int villaId)
+        {
+            var response = await _villaService.GetAsync<APIResponse>(villaId);
+            if (response != null && response.IsSuccess)
+            {
+                VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteVilla(VillaDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
+        }
+
     }
 }
