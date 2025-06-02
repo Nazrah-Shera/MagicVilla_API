4553ed3a2d06b7a0c79c49c66071258a34f00992
Nazrah Shera <nazrah.shera@gmail.com>
2025-06-02 19:52:43 +0500

    Create Villa UI

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
+using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
@@ -18,16 +19,37 @@ namespace MagicVilla_Web.Controllers
             _mapper = mapper;
             
         }
-        public  async Task<IActionResult> IndexVilla()
+        public async Task<IActionResult> IndexVilla()
         {
             List<VillaDTO> list = new();
             var response = await _villaService.GetAllAsync<APIResponse>();
-            if (response != null && response.IsSuccess) 
+            if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
-                 
+
             }
             return View(list);
+        }
+        public  async Task<IActionResult> CreateVilla()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
         }
     }
 }
